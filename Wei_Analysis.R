5a921d204300997cdb436f8306fdb41eee3869bd
chbianco <christopherhbianco@gmail.com>
2024-06-17 10:10:27 -0400

    Update Wei_Analysis.R

--- Wei_Analysis.R
+++ Wei_Analysis.R
@@ -3,7 +3,41 @@
 library(dplyr)
 library(ggplot2)
 soilC <- read.csv(file = 'GCAM_soilC.csv')
-PostKwon <- read.csv(file= 'Experimental Data.csv', na.strings = c("", "NA"))
+Wei <- read.csv(file= 'Wei et al Data.csv', na.strings = c("", "NA"))
 timescales <- read.csv(file = 'soil_timescales.csv')
 glus <- read.csv(file = 'GLU_codes.csv')
 regions <- read.csv('GCAM_regions.csv')
+
+
+#Join GLU codes with soilC data
+soilC %>%
+  mutate(GLU_code = GLU) %>%
+  right_join(glus, by='GLU_code') %>%
+  right_join(regions, by='GCAM_region_ID') %>%
+  right_join(timescales, by='GCAM_region_ID')-> soilC_regions
+
+
+#Simplify the data to just the stuff we'll need for the Post & Kwon comparisons 
+soilC_regions %>%
+  select(Land_Type, soil_c, GLU_code, soilTimeScale, GCAM_region_ID) -> simple_soilC_regions
+
+#Matching experimental data to GCAM data
+Wei %>%
+  select(Initial_Land_Use, Final_Land_Use, GLU_code, GCAM_region_ID, OC_decrease) %>%
+  na.omit() %>%
+  mutate(Land_Type = Initial_Land_Use) %>%
+  right_join(simple_soilC_regions, by = c('GLU_code', 'Land_Type', 'GCAM_region_ID')) %>%
+  rename(initial_soil_c = soil_c) %>%
+  select(-Land_Type) %>%
+  mutate(Land_Type = Final_Land_Use) %>%
+  right_join(simple_soilC_regions, by = c('GLU_code', 'Land_Type', 'GCAM_region_ID')) %>%
+  rename(final_soil_c = soil_c) %>%
+  select(-Land_Type, -soilTimeScale.x) %>%
+  rename(soilTimeScale = soilTimeScale.y) %>%
+  na.omit() %>%
+  mutate(GCAM_Rate = (final_soil_c - initial_soil_c)/soilTimeScale, Rate_Difference = Exp_Rate - GCAM_Rate, 
+         Exp_k = -log(abs(Exp_Rate)*Time +1)/Time,
+  ) %>%
+  #This next line corrects the sign of Exp_k--we had to take the absolute value to avoid NaNs, so this accounts for that 
+  mutate(Exp_k = ifelse(sign(Exp_k) == sign(Exp_Rate), Exp_k*(-1), Exp_k)) -> Rate_Comparison
+
